b131981dd1a9f9693d51c12aeaad8a17667dffc3
mplaczek99 <mplaczek99@gmail.com>
2026-02-18 20:31:09 -0600

    Added a basic game loop

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
 
 
 def main():
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    # Start the game
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Game loop
+    while True:
+        log_state()
+
+        # Process event queue
+        for event in pygame.event.get():
+            # Exit the game if window is closed
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
