7cf6b04519e8f3ad581decbe2157b683d0d7e279
mplaczek99 <mplaczek99@gmail.com>
2026-02-21 23:54:38 -0600

    Added logic so asteroids and shots collide, they disappear

--- main.py
+++ main.py
@@ -52,6 +52,13 @@ def main():
         updatable.update(dt)
 
         for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    asteroid.kill()
+                    shot.kill()
+
+        for asteroid in asteroids:
             if asteroid.collides_with(player):
                 log_event("player_hit")
                 print("Game over!")
